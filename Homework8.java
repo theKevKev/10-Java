9f3b26c9c9c4ea7cc6da4e5a594de639ae00b2f3
theKevKev <kevinhan.119@gmail.com>
2021-01-27 20:20:26 -0600

    Jan 27, 2021

---
+++ Homework8.java
@@ -0,0 +1,30 @@
+public class Homework8 {
+    
+    public static final int NumOfFibonacci = 12;
+    public static final int NumOfLines = 5;
+    public static void main(String[] args){
+        
+        //Question 3
+
+        System.out.println("\nQuestion 3: \n");        
+        int Num1 = 1, Num2 = 1;
+        System.out.print(Num1 + " ");
+        for(int num = 1; num <= NumOfFibonacci - 1; num++){
+            System.out.print(Num1 + " ");
+            Num1 = Num1 + Num2;
+            Num2 = Num1 - Num2;
+        }
+        System.out.println();
+        
+        //Question 5
+
+        System.out.println("\nQuestion 5: \n");
+        for(int line = 1; line <= NumOfLines; line++){
+            for(int i = 1; i <= line; i++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        System.out.println("\n\nProblems 9, 11, and 15 are on the other link from last class' homework.\n\n");
+    }
+}
