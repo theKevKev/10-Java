7259fa81050419925d831a65f91256fd56ecda24
theKevKev <kevinhan.119@gmail.com>
2021-02-04 13:08:47 -0600

    February 4, 2021 Commit

--- Chapter2Projects/Chapter2Project4.java
+++ Chapter2Projects/Chapter2Project4.java
@@ -4,7 +4,7 @@ public class Chapter2Project4 {
     public static final int SIZE = 5;
 
     public static void main(String[] args){
-        for(int layer = 1; layer <= SIZE; layer++){
+        for(int layer = 1; layer <= SIZE; layer++){ //Loops per layer
             HeadLine(layer);
             BodyLine(layer);
             FeetLine(layer);
@@ -38,12 +38,12 @@ public class Chapter2Project4 {
         }
         System.out.println();
     }
-    public static void Spaces(int layer){
+    public static void Spaces(int layer){ //These are the spaces before the person
         for(int j = 1; j <= 5 * SIZE - 5 * layer; j++){
             System.out.print(" ");
         }
     }
-    public static void AntiSpaces(int layer){
+    public static void AntiSpaces(int layer){ //These are the spaces after the person
         for(int j = 1; j <= 5 * layer - 5; j++){
             System.out.print(" ");
         }
---
+++ Chapter2Projects/Chapter2Project7.java
@@ -0,0 +1,67 @@
+package Chapter2Projects;
+
+public class Chapter2Project7 {
+    public static final int SIZE = 10;
+    public static void main(String[] args){
+        for(int i = 1; i <= SIZE + 1; i++){
+            System.out.print(" ");
+        }
+        HorizontalLine();
+        System.out.println();
+        Book();
+        HorizontalLine();
+        for(int i = 1; i <= SIZE; i++){
+            System.out.print("/");
+        }
+        System.out.println();
+        Pages();
+        HorizontalLine();
+        System.out.println();
+    }
+    public static void HorizontalLine(){
+        System.out.print("+");
+        for(int i = 1; i <= 3 * SIZE; i++){
+            System.out.print("-");
+        }
+        System.out.print("+");
+    }
+    public static void Book(){
+        for(int line = 1; line <= SIZE; line++){
+            for(int space = 1; space <= SIZE + 1 - line; space++){
+                System.out.print(" ");
+            }
+            System.out.print("/");
+            for(int longspace = 1; longspace <= 3 * SIZE - 3 * line; longspace++){
+                System.out.print(" ");
+            }
+            System.out.print("_");
+            for(int repeat = 1; repeat <= line; repeat++){
+                System.out.print("__/");
+            }
+            for(int slash = 1; slash <= line - 1; slash++){
+                System.out.print("/");
+            }
+            System.out.println();
+        }
+    }
+    public static void Pages(){
+        for(int line = 1; line <= SIZE / 2; line++){
+            System.out.print("|");
+            for(int space = 1; space <= (3 * SIZE - 22) / 2; space++){
+                System.out.print(" ");
+            }
+            System.out.print("Building Java Programs");
+            for(int space = 1; space <= (3 * SIZE - 22) / 2; space++){
+                System.out.print(" ");
+            }
+            if(SIZE % 2 == 1) {
+                System.out.print(" ");
+            }
+            System.out.print("|");
+            for(int slash = 1; slash <= 2 + SIZE - 2 * line; slash++){
+                System.out.print("/");
+            }
+            System.out.println();
+        }
+    }
+}
