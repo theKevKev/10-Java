86ae1ef992a9e5fca6bb76183c73916f51a49172
theKevKev <kevinhan.119@gmail.com>
2021-02-26 12:33:11 -0600

    February 26, 2021 Commit 1

---
+++ Chapter4Exercise3.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Chapter4Exercise3 {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        boolean variable = true;
+        do{
+            System.out.println("Month?: ");
+            int month = input.nextInt();
+            System.out.println("Date?: ");
+            int day = input.nextInt();
+            String season = Season(month, day);
+        
+            if(season.equals("false") == false){
+                System.out.println("This date is during the season of " + season + ".");
+                variable = false;
+            } else{
+                System.out.println("The entered date of " + month + "/" + day + " is not a valid date. Please enter a valid date:");
+            }
+        } while(variable == true);
+        input.close();
+    }
+    public static String Season(int m, int d){
+        if((m == 12 && d >= 16 && d <= 31) || (m == 1 && d >= 1 && d <= 31) || (m == 2 && d >= 1 && d <= 29) || (m == 3 && d <= 15 && d >= 1)){
+            return ("winter");
+        } else if((m == 3 && d >= 16 && d <= 31) || (m == 4 && d >= 1 && d <= 30) || (m == 5 && d >= 1 && d <= 31) || (m == 6 && d <= 15 && d >= 1)){
+            return ("spring");
+        } else if((m == 6 && d >= 16 && d <= 30) || (m == 7 && d >= 1 && d <= 31) || (m == 8 && d >= 1 && d <= 31) || (m == 9 && d <= 15 && d >= 1)){
+            return ("summer");
+        } else if((m == 9 && d >= 16 && d <= 30) || (m == 10 && d >= 1 && d <= 31) || (m == 11 && d >= 1 && d <= 30) || (m == 12 && d <= 15 && d >= 1)){
+            return ("spring");
+        } else {
+            return ("false");
+        }
+    }
+}
+
