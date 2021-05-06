6853b0d5f34af0e83961b9e18b6f2ea3987edc5f
theKevKev <kevinhan.119@gmail.com>
2021-05-06 11:35:19 -0500

    May 6, 2021 Commit 1

--- test3.java
+++ test3.java
@@ -1,11 +1,11 @@
-import java.util.*;
+// import java.util.*;
 public class test3 {
     public static void main(String[] args){
         int grade1 = 3;
         int grade2 = 4;
-        int grade3 = 1;
+        // int grade3 = 1;
         Student Kevin = new Student(grade1, grade2);
-        System.out.println(Kevin.getGrade1);
+        System.out.println(Kevin.getGrade1());
         Math.random();
     }
 }
